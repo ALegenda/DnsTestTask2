55a69556729c6d7f60bbefe3d13b7e13bab86c7d
TomKuper <adalekseev@edu.hse.ru>
2019-07-31 20:56:58 +1000

    Add trim " for -s argument

--- DnsTestTask2/Program.cs
+++ DnsTestTask2/Program.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DnsTestTask2
@@ -10,12 +10,62 @@ namespace DnsTestTask2
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            var path = ".";
+            var maxThreads = 10;
+            var substring = "test";
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            if (args.Length > 0)
+            {
+
+                path = args[0];
+
+                for (var i = 1; i < args.Length; i++)
+                {
+                    switch (args[i])
+                    {
+                        case "-t":
+                            maxThreads = int.Parse(args[i + 1]);
+                            break;
+                        case "-s":
+                            substring = args[i + 1].Trim('"');
+                            break;
+                    }
+                }
+
+            }
+
+            var fileNames = Directory.EnumerateFiles(path);
+
+
+            Parallel.ForEach(
+                fileNames,
+                new ParallelOptions { MaxDegreeOfParallelism = maxThreads },
+                fileName => FindString(fileName, substring)
+                );
+
+        }
+
+        private static void FindString(string fileName, string substring)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var file = File.ReadAllLines(fileName);
+            for (var i = 0; i < file.Length; i++)
+            {
+                if (file[i].Contains(substring))
+                {
+                    PrintInfo(fileName, sw.ElapsedTicks, file[i], i);
+                }
+            }
+
+            sw.Stop();
+        }
+
+        private static void PrintInfo(string fileName, long tiks, string line, long lineNumber)
+        {
+            var ns = 1000000000.0 * tiks / Stopwatch.Frequency;
+            Console.WriteLine($"{fileName} [thread={Thread.CurrentThread.ManagedThreadId},time={Convert.ToInt64(ns)}ns, line={lineNumber}] {line}");
         }
     }
 }
